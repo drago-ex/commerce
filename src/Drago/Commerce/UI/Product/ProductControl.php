9f215b85f214be906b1db1a0ffedbc65dc19d053
accgit <zdenek.papucik@gmail.com>
2025-06-24 06:35:53 +0200

    dev

--- src/Drago/Commerce/UI/Product/ProductControl.php
+++ src/Drago/Commerce/UI/Product/ProductControl.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace Drago\Commerce\UI\Product;
 
+use Brick\Money\Currency;
 use Brick\Money\Exception\UnknownCurrencyException;
 use Brick\Money\Money;
 use Dibi\Exception;
@@ -16,6 +17,7 @@ use Drago\Application\UI\ExtraControl;
 use Drago\Attr\AttributeDetectionException;
 use Drago\Commerce\Commerce;
 use Drago\Commerce\Domain\Product\Product;
+use Drago\Commerce\Domain\Product\ProductEntity;
 use Drago\Commerce\Domain\Product\ProductRepository;
 use Drago\Commerce\Service\ShoppingCartSession;
 use Drago\Commerce\UI\Factory;
@@ -68,16 +70,32 @@ class ProductControl extends ExtraControl
 	 * @throws Exception
 	 * @throws AttributeDetectionException
 	 */
-	public function success(Form $form): void
+	public function success(Form $form, ProductData $data): void
 	{
-		$productId = $form->getValues()['productId'];
-		$row = $this->productRepository->getOne($productId);
-		$product = new Product(
-			id: $row->id,
-			name: $row->name,
-			price: Money::of($row->price, $this->commerce->moneyZero()),
+		$product = $this->getProduct($data->productId);
+		$item = new Product(
+			id: $product->id,
+			name: $product->name,
+			price: Money::of($product->price, $this->getCurrency()),
 		);
-		$this->shoppingCartSession->addItem($product);
+		$this->shoppingCartSession->addItem($item);
 		$this->redirect('this');
+	}
+
+
+	private function getCurrency(): Currency
+	{
+		return $this->commerce->moneyZero()
+			->getCurrency();
+	}
+
+
+	/**
+	 * @throws Exception
+	 * @throws AttributeDetectionException
+	 */
+	private function getProduct(int $productId): array|ProductEntity
+	{
+		return $this->productRepository->getOne($productId);
 	}
 }
---
+++ src/Drago/Commerce/UI/Product/ProductData.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Drago Extension
+ * Package built on Nette Framework
+ */
+
+declare(strict_types=1);
+
+namespace Drago\Commerce\UI\Product;
+
+
+class ProductData
+{
+	public int $productId;
+}
